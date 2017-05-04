73f1eb178172d50cc02c49fd43aceb321bb26b74
Neel Mallepally <iNeel@Neel-Mallepallys-MacBook-Pro.local>
2017-05-04 08:21:02 -0400

    Initial layout very basic version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,37 @@ import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './home/home.component';
+
+import { LayoutBodyComponent } from './layout/body/body.component';
+import { LayoutHeadComponent } from './layout/layout-head/layout-head.component';
+import { LayoutContentComponent } from './layout/layout-content/layout-content.component';
+import { LayoutFooterComponent } from './layout/layout-footer/layout-footer.component';
+import { MenuComponent } from './layout/menu/menu.component';
+
+const appRoutes : Routes = [
+  {path: "pages", component: PagesComponent },
+  {path: 'home', component: HomeComponent},
+  {path:'', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    HomeComponent,
+    LayoutBodyComponent,
+    LayoutHeadComponent,
+    LayoutContentComponent,
+    LayoutFooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/layout-head/layout-head.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'layout-head',
+  templateUrl: './layout-head.component.html',
+  styleUrls: ['./layout-head.component.css']
+})
+export class LayoutHeadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
